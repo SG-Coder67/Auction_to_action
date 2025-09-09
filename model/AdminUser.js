76c71816b6a2efa1766202fce00125f17f009879
adithyan-css <adithyansubramani1@gmail.com>
2025-09-09 20:14:12 +0530

    Update AdminUser.js

--- model/AdminUser.js
+++ model/AdminUser.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
 const adminUserSchema = new mongoose.Schema({
+  // Username field: must be unique, required, and trimmed (removes whitespace)
   username: { 
     type: String, 
     required: true, 
     unique: true,
     trim: true 
   },
+  // Password field: required, will be stored as a hashed value
   password: { 
     type: String, 
     required: true 
   },
+  // Role field: defaults to "admin" (useful if later we add other roles)
   role: { 
     type: String, 
     default: 'admin' 
@@ -24,5 +27,5 @@ adminUserSchema.methods.toJSON = function() {
   delete adminObject.password;
   return adminObject;
 };
-
+// Export the model for use in controllers and routes
 module.exports = mongoose.model('AdminUser', adminUserSchema);
